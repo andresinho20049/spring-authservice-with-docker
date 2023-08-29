e41a1895063411f5b8f88155e6076ed0cb8717a9
André Carlos <andresinho200498@gmail.com>
2023-08-29 20:24:33 -0300

    Use enum and printLog on debug

--- src/main/java/com/andresinho20049/authservice/config/DevConfig.java
+++ src/main/java/com/andresinho20049/authservice/config/DevConfig.java
@@ -8,13 +8,16 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.andresinho20049.authservice.enums.RolesEnum;
 import com.andresinho20049.authservice.exceptions.ProjectException;
 import com.andresinho20049.authservice.models.Roles;
 import com.andresinho20049.authservice.models.User;
 import com.andresinho20049.authservice.service.RolesService;
 import com.andresinho20049.authservice.service.UserService;
-import com.andresinho20049.authservice.utils.Constants;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Configuration
 @Profile({ "dev" })
 @ComponentScan({ "com.andresinho20049.authservice.controller" })
@@ -27,18 +30,12 @@ public class DevConfig {
 	private RolesService rolesService;
 
 	@Bean
-	public void startDatabase() {
-		this.initialUser();
-	}
+	void startDatabase() {
+		log.debug("################ Start with Profile Dev ################");
 
-	private void initialUser() {
-
-		String[] rolesName = { Constants.ROLE_ADMIN, Constants.ROLE_VIEW_USER, Constants.ROLE_CREATE_USER,
-				Constants.ROLE_UPDATE_USER, Constants.ROLE_UPDATE_ROLES_USER, Constants.ROLE_DELETE_USER,
-				Constants.ROLE_DISABLE_USER };
-		
 		Roles roles = null;
-		for (String roleName : rolesName) {
+		for (RolesEnum role : RolesEnum.values()) {
+			String roleName = role.name();
 			try {
 				rolesService.findByName(roleName);
 			} catch (ProjectException e) {
@@ -46,15 +43,14 @@ public class DevConfig {
 				rolesService.save(roles);
 			}
 		}
-		
+
 		String supportMail = "admin@email.com";
 		try {
 			userService.findByEmail(supportMail, true);
 		} catch (ProjectException e) {
-			roles = rolesService.findByName(Constants.ROLE_ADMIN);
-			User user = new User("Admin", supportMail, "password@1234", Arrays.asList(roles));
+			roles = rolesService.findByName(RolesEnum.ROLE_ADMIN.name());
+			User user = new User("Admin", supportMail, "strongPassword@1234", Arrays.asList(roles));
 			userService.save(user);
 		}
-
 	}
 }
