55eac2f6bcaf051ca01acab71da59d076255feaa
André Carlos <andresinho200498@gmail.com>
2023-08-28 22:27:58 -0300

    Repository annotation

--- src/main/java/com/andresinho20049/authservice/repository/RoleRepository.java
+++ src/main/java/com/andresinho20049/authservice/repository/RoleRepository.java
@@ -3,10 +3,11 @@ package com.andresinho20049.authservice.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.andresinho20049.authservice.models.Roles;
 
-
+@Repository
 public interface RoleRepository extends JpaRepository<Roles, Long> {
 
     Optional<Roles> findByName(String name);
