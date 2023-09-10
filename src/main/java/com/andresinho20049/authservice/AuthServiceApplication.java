c254f74f5a2c28d2699e46b97d6c9085f8156f27
André Carlos <andresinho200498@gmail.com>
2023-09-10 16:45:14 -0300

    AddShutdownHook

--- src/main/java/com/andresinho20049/authservice/AuthServiceApplication.java
+++ src/main/java/com/andresinho20049/authservice/AuthServiceApplication.java
@@ -4,12 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 @EnableAutoConfiguration
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
+		
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				log.debug("Shutdown hook is running...");
+			}
+		});
+		
 	}
 
 }
